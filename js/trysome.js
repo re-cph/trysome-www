95819d4530f353ef1e0990ded7b67da91b9ca921
vesters <v@re-cph.com>
2015-05-07 19:00:22 +0200

    Beta 1

--- js/trysome.js
+++ js/trysome.js
@@ -1,11 +1,3 @@
-$(function() {
-  // GOTO ABOUT
-  $("#goto-about").on('click', function() {
-    $(window).scrollTo($("#about").offset().top - 60, 1000);
-  });
+(function ($) {
 
-  // GOTO SIGNUP
-  $("#goto-signup").on('click', function() {
-    $(window).scrollTo($("#signup").offset().top - 60, 1500);
-  });
-});
+})(jQuery);
